61195590a3f1fcdc89f7029b0fc427ee653f7817
glovebx <glovebx@glovebxdeMacBook-Pro-2.local>
2024-08-13 22:59:51 +0800

    renew

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -1,6 +1,8 @@
 import { subscribeNewsletterSchema } from "@/lib/types";
 import { NextResponse } from "next/server";
 
+export const runtime = 'edge';
+
 export async function POST(request: Request) {
   const body: unknown = await request.json();
 
